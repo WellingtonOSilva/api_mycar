d1d4098dead7661c1c23a69353eb3f6a6956a74c
Wellington Silva <wellington.spwos@outlook.com>
2018-12-20 23:28:32 -0200

    Added create user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use \App\Model\Users as Users;
+use Illuminate\Http\Request;
+
 
 class UsersController extends Controller
 {
@@ -11,9 +13,16 @@ class UsersController extends Controller
      *
      * @return void
      */
+    private $structure;
+
     public function __construct()
     {
-        //
+        $this->structure = [
+            'email'         => 'required|email',
+            'name'          => 'required',
+            'password'      => 'required',
+            'permission'    => 'required'
+        ];
     }
 
     public function all() {
@@ -24,6 +33,24 @@ class UsersController extends Controller
         return Users::where('id_user', $id)->get();
     }
 
+    public function create(Request $request) {
 
+        if(!$validation = $this->validate($request, $this->structure)) {
+            return $validation;
+        } 
+
+        $user = new Users();
+
+        $user->email        = $request->input('email');
+        $user->name         = $request->input('name');
+        $user->password     = $request->input('password');
+        $user->permission   = $request->input('permission');
+
+        if(!$user->save()) {
+            return ["error" => "Não foi possivel"];
+        } 
+
+        return $this->returnSuccess($user->id_user, "User created succesfully", $request->except(['password']));
+    } 
     //
 }
