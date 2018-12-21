e70376500f1cb376ddd563d7c31a72a1e1e84e21
Wellington Silva <wellington.spwos@outlook.com>
2018-12-20 23:27:50 -0200

    Added functions to return

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,4 +7,11 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     //
+    public function returnSuccess($id, $message, $data) {
+        return [
+            'id'        => $id,
+            'success'   => $message,
+            'data'      => $data
+        ];
+    }
 }
